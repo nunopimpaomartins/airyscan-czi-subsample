e975ba1db6e710489b71ab9ffc95ac4ba4a907a2
Nuno Pimpao Martins <nuno.pimpao@gmail.com>
2025-05-20 12:13:00 +0100

    script to split tiles in airyscan processed data

---
+++ splitsave_czi_tile.py
@@ -0,0 +1,97 @@
+import os
+from pathlib import Path
+import argparse
+import tqdm
+
+from bioio import BioImage
+import bioio_czi
+import pylibCZIrw as pyczi
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--dataPath", help="The path to your data")
+parser.add_argument("--extension", help="The extension of the files to be processed", default='.czi')
+
+args = parser.parse_args()
+print(args.dataPath)
+
+if args.dataPath is None:
+    print("Please provide a data path")
+    exit(1)
+
+basedir = Path(args.dataPath)
+
+def main(datapath='.', extension='.tif'):
+    filelist = os.listdir(datapath)
+
+    filelist = [f for f in filelist if f.find(extension) > 0]
+    filelist.sort()
+    print('Nr of czi files in dir:', len(filelist))
+
+    savedir = Path(str(basedir) + '/split_czi/')
+    savedir.mkdir(parents=True, exist_ok=True)
+    print('Saving output to:', savedir)
+
+    for file in tqdm.tqdm(filelist):
+        filepath = str(datapath) + '/' + file
+        filename_noext = file[:file.index(extension)]
+        print('Processing file:', filepath)
+
+        img = BioImage(
+            filepath,
+            reader=bioio_czi.Reader,
+            reconstruct_mosaic=False,
+            include_subblock_metadata=True,
+            use_aicspylibczi=True,
+        )
+        print('Image dimensions:', img.dims)
+
+        if img.dims.order.find('M') < 0:
+            print('Image is not mosaic, skipping')
+            continue
+
+        for scene in img.scenes:
+            img.set_scene(scene)
+            print('Current scene:', scene)
+
+            n_tiles = img.dims['M'][0]
+
+            scale_x = img.scale.X
+            scale_y = img.scale.Y
+            scale_z = img.scale.Z
+
+            for tile in range(n_tiles):
+                img_data = img.get_image_dask_data(img.dims.order[1:], M=tile) #TODO better strategy to exclude dimension to split
+                img_data_tile = img_data.compute()
+
+                ch_names = {}
+                for i in range(len(img.channel_names)):
+                    ch_names[i] = img.channel_names[i]
+
+                tile_save_path = str(savedir) + '/' + filename_noext + '_tile' + str(tile+1).zfill(2) + '.czi'
+                print('Tile save path:', tile_save_path)
+
+                with pyczi.create_czi(tile_save_path, exist_ok=True) as czidoc_w:
+                    for t in range(img.dims['T'][0]):
+                        for c in range(img.dims['C'][0]):
+                            for z in range(img.dims['Z'][0]):
+                                temp_image = img_data_tile[t, c, z, ...]
+                                czidoc_w.write(
+                                    data=temp_image,
+                                    plane={
+                                        'T': t,
+                                        'C': c,
+                                        'Z': z,
+                                    },
+                                    compression_options = "zstd0:ExplicitLevel=2"
+                                )
+                
+                czidoc_w.write_metadata(
+                    filename_noext + '_tile' + str(tile+1).zfill(2),
+                    channel_names=ch_names,
+                    scale_x=scale_x,
+                    scale_y=scale_y,
+                    scale_z=scale_z,
+                )
+
+
+main(basedir, args.extension)
