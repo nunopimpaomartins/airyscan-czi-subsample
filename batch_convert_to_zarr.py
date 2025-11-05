4596c17cf474e4ff0a70050a6732c8a0c67262ae
Nuno Pimpao Martins <nuno.pimpao@gmail.com>
2025-11-05 10:34:23 +0000

    replace loading data with pylibczirw instead of bioio

--- batch_convert_to_zarr.py
+++ batch_convert_to_zarr.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import argparse
 from tqdm import tqdm
+import numpy as np
 
 import dask.array as da 
 from multiview_stitcher import spatial_image_utils as si_utils
@@ -21,8 +22,9 @@ basedir = Path(args.dataPath)
 
 # get the image reader based on the file extension
 if args.extension == '.czi':
-    from bioio import BioImage
-    import bioio_czi
+    # from bioio import BioImage
+    # import bioio_czi
+    from pylibCZIrw import czi as pyczi
 else:
     from tifffile import imread
 
@@ -43,14 +45,25 @@ def main(datapath='.', extension='.czi'):
 
         # Getting image data voxel scales
         file_path = str(datapath / file)
-        img = BioImage(
-            file_path,
-            reader=bioio_czi.Reader,
-            reconstruct_mosaic=False,
-            include_subblock_metadata=True,
-            use_aicspylibczi=True
-        )
-        scale = {'z': img.scale.Z, 'y': img.scale.Y, 'x': img.scale.X}
+        # img = BioImage(
+        #     file_path,
+        #     reader=bioio_czi.Reader,
+        #     reconstruct_mosaic=False,
+        #     include_subblock_metadata=True,
+        #     use_aicspylibczi=True
+        # )
+        # scale = {'z': img.scale.Z, 'y': img.scale.Y, 'x': img.scale.X}
+        with pyczi.open_czi(file_path) as czidoc:
+            md_dic = czidoc.metadata
+            pixelsize_x = float(md_dic['ImageDocument']['Metadata']['Scaling']['Items']['Distance'][0]['Value'])
+            pixelsize_y = float(md_dic['ImageDocument']['Metadata']['Scaling']['Items']['Distance'][1]['Value'])
+            pixelsize_z = float(md_dic['ImageDocument']['Metadata']['Scaling']['Items']['Distance'][2]['Value'])
+            
+        pixelsize_x = pixelsize_x / 10**-6  # convert scale from microns to meters
+        pixelsize_y = pixelsize_y / 10**-6  # convert scale from microns to meters
+        pixelsize_z = pixelsize_z / 10**-6  # convert scale from microns to meters
+
+        scale = {'z': pixelsize_z, 'y': pixelsize_y, 'x': pixelsize_x}
         print('Voxel scales: %s' % scale)
         
         overwrite = True
@@ -61,8 +74,20 @@ def main(datapath='.', extension='.czi'):
         zarr_path = os.path.join(savedir, file_savename)
         
         # get data dimensions without T axis from metadata
-        im_data = img.get_image_data(img.dims.order[img.dims.order.index('T')+1:])
+        # im_data = img.get_image_data(img.dims.order[img.dims.order.index('T')+1:])
+        with pyczi.open_czi(file_path) as cziimg:
+                        tbd = cziimg.total_bounding_box
+                        im_data = np.zeros((tbd['C'][1], tbd['Z'][1], tbd['Y'][1], tbd['X'][1]))
 
+                        for t in range(tbd['T'][1]):
+                            for c in range(tbd['C'][1]):
+                                for z in range(tbd['Z'][1]):
+                                    temp = cziimg.read(
+                                        plane = {'C': c, "T": t, "Z": z},
+                                        scene = 0,
+                                    )
+                                    im_data[c, z] = temp.squeeze()
+        
         sim = si_utils.get_sim_from_array(
             im_data,
             dims=["c", "z", "y", "x"],
